35f8515352988f0574c65a72e00c2373b0cf64a6
Sameony <iam2001sam@gmail.com>
2021-09-07 12:14:14 +0530

    JS complete

---
+++ app.js
@@ -0,0 +1,32 @@
+//inputs
+var btn = document.querySelector("#btn-check");
+var txt_input = document.querySelector("#txt-input");
+var txt_output = document.querySelector("#txt-output");
+var bonana_url = "https://api.funtranslations.com/translate/minion.json";
+
+//processes
+function makeMyUrl(text)
+{
+    return(bonana_url+"?"+"text="+text);
+}
+
+function errorHandler(error)
+{
+    alert("Unexpected error: "+error);
+}
+
+function fetchit(url)
+{
+    fetch(url)
+    .then(response=>response.json)
+    .then(json=>txt_output.innerText(json.contents.translated))               //output
+    .catch(errorHandler)
+}
+
+function helper(){
+    console.log("Voila!");
+    fetchit(makeMyUrl(txt_input.value));
+}
+
+btn = btn.addEventListener("click",helper);
+
